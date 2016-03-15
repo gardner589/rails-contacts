a7b7ef0903b34c7c812d7e71c420d4ee4e1ad019
Nick Olds <nolds9@yahoo.com>
2016-03-15 12:50:02 -0400

    updates app dependencies, switches to postgres for db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140122195144) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "people", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
