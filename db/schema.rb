e4b5832e566a2d4d4da8c1a8b1b2a631f8b9dacf
Nick Olds <nolds9@yahoo.com>
2016-03-16 13:02:58 -0400

    removes unnecessary migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160315192143) do
+ActiveRecord::Schema.define(version: 20140123155138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
